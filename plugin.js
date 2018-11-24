3fe6021e385cac58312dc7810ef1d05077f5ee86
web-west <admin@app.com>
2018-11-24 13:18:35 +0200

    [*] Fix bugs router

--- plugin.js
+++ plugin.js
@@ -1,30 +1,15 @@
 export default ({ app: { router } }) => {
-  let ready = false
-
-  router.onReady(() => {
-    // Mark when the router has completed the initial navigation.
-    ready = true
-  })
-
   function create() {
-    window['vkCounter<%= options.id.replace(/[^A-Za-z0-9]/gim,"") %>'] = new VK.Retargeting.Init('<%= options.id.replace(/[^A-Za-z0-9]/gim,"") %>')
-
+    VK.Retargeting.Init('<%= options.id %>')
     router.afterEach((to, from) => {
-      if (!ready) {
-        // Don't record a duplicate hit for the initial navigation.
-        return
-      }
-      window['vkCounter<%= options.id.replace(/[^A-Za-z0-9]/gim,"") %>'].hit(to.fullPath, {
-        referer: from.fullPath
-      })
+      VK.Retargeting.Hit()
     })
   }
-
-  if (window.VK === undefined) {
+  if (window.VK && window.VK.Retargeting) {
     create()
   } else {
-    window.VK.Retargeting.Hit()
+    (function (w, c) {
+      (w[c] = w[c] || []).push(create)
+    })(window, 'vk_callbacks')
   }
-
-
 }
